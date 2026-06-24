d519a157a5573aa86a71b3abfc144685e39fd6f9
Daniel <daniel1antonyy@gmail.com>
2026-06-23 20:48:12 -0500

    chore: register new resource modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CurrenciesModule } from './currencies/currencies.module';
+import { UsersModule } from './users/users.module';
+import { HouseholdsModule } from './households/households.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CurrenciesModule, UsersModule, HouseholdsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
