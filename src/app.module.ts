d93f002415cdf1d6823a353f041373f007ee2ff9
Daniel <daniel1antonyy@gmail.com>
2026-06-24 18:42:40 -0500

    chore: register new modules and configure main application with swagger and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { CurrenciesModule } from './currencies/currencies.module';
 import { UsersModule } from './users/users.module';
 import { HouseholdsModule } from './households/households.module';
 import { CategoriesModule } from './categories/categories.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { RecurringConfigsModule } from './recurring-configs/recurring-configs.module';
+import { BudgetsModule } from './budgets/budgets.module';
+import { SavingsGoalsModule } from './savings-goals/savings-goals.module';
 
 @Module({
-  imports: [PrismaModule, CurrenciesModule, UsersModule, HouseholdsModule, CategoriesModule],
+  imports: [PrismaModule, CurrenciesModule, UsersModule, HouseholdsModule, CategoriesModule, TransactionsModule, RecurringConfigsModule, BudgetsModule, SavingsGoalsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Expellio API')
+    .setDescription('The Expellio API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
